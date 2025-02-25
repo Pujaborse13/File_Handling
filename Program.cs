2b9655c42c045f7f246cea420d196af78b8d5f42
Puja Borse <pujaborse13@gmail.com>
2025-02-25 16:52:22 +0530

    Main Claas Added

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using File_IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //File Exist or not
+        FileOperations fe = new FileOperations();
+        fe.FileExsitsOrNot();
+
+        //File Read
+        FileOperations fRead = new FileOperations();
+        fRead.ExistingFileRead();
+
+        //Copy File
+        FileOperations fCopy = new FileOperations();
+        fCopy.CopyFile();
+
+        //Delete File
+        FileOperations fDelete = new FileOperations();
+        fDelete.DeleteFile();
+
+        
+    }
+}
